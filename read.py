34666515b855aecb47e6115718fc927cc6f19409
Tony Lin <tonylinquant@gmail.com>
2021-08-08 19:18:00 +0800

    Add list comprehension

--- read.py
+++ read.py
@@ -22,3 +22,18 @@ for d in data:
 		new.append(d)
 print('一共有', len(new), '筆留言長度<100')
 print(new[1])
+
+# 找出包含'good'的留言
+#good = []
+#for d in data:
+#	if 'good' in d:
+#		good.append(d)
+#print('一共有', len(good), '筆留言提到good')
+#print(good[0])
+
+# 清單快寫法  (運算 變數 清單 篩選條件)
+good = [d for d in data if 'good' in d]
+print(good)
+
+bad = ['bad' in d for d in data]
+print(bad)
