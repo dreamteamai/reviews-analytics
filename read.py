edf464d8fbcd2caa2f9a055066a1ab8f1ec61f16
Tony Lin <tonylinquant@gmail.com>
2021-08-08 18:56:16 +0800

    add filtering function

--- read.py
+++ read.py
@@ -14,3 +14,11 @@ for message in data:
 	sum_len = sum_len + len(message)
 avaerage_len = sum_len / len(data)
 print('留言平均長度為', avaerage_len)	
+
+# 找出長度<100的留言 
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度<100')
+print(new[1])
